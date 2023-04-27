e7907623e5959e494dc9021eb6984b0f1dc6769c
Emiko Bell <emikoelizabethbell@gmail.com>
2023-04-27 14:49:43 -0700

    Completed lab 1 exercise

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,15 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+  <React.Fragment>
+    <h2>Hello! Welcome to our card game!</h2>
+    <a href='/cards'>Click here to view the cards</a>
+    <div>
+      <img src="/static/img/balloonicorn.jpg" />
+    </div>
+  </React.Fragment>
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
